3012585c27b0c86e0f4ddccd1df904bcd5e25e01
GlebGukov <Gleb_Gukov@inbox.ru>
2023-03-16 14:25:42 +0300

    added delete and edit methods

--- src/main/java/com/example/coursework/controllers/HeaderController.java
+++ src/main/java/com/example/coursework/controllers/HeaderController.java
@@ -5,10 +5,7 @@ import com.example.coursework.repositories.PostNewsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 
 @Controller
@@ -18,34 +15,66 @@ public class HeaderController {
     private PostNewsRepository postNewsRepository;
 
     @GetMapping("/news/{title}")
-    public String technology(@PathVariable(name = "title") String typeOfNews, Model model){
-        model.addAttribute("title",typeOfNews);
-        Iterable<PostNews> postNews = postNewsRepository.findByTitleContainingOrAnonsContaining("#"+typeOfNews,"#"+typeOfNews);
-        model.addAttribute("worldNews",postNews);
-        return "newsHeader";
-    }
-    @GetMapping("/newAdd")
-    public String newsAdd(Model model){
-
-        return "newsAdd";
-    }
-//    @GetMapping("/news/${id}")
-//    public String newsDetails(@PathVariable(name = "id") long id , Model model){
-//        PostNews postNews = postNewsRepository.findById(id).orElse(null);
-//        model.addAttribute("news",postNews);
-//        return "newsDetails";
-//    }
-
-    @GetMapping("/aboutme")
-    public String aboutMe(Model model){
-        model.addAttribute("title","About Me");
-        return "aboutMe";
+    public String news(@PathVariable(name = "title") String typeOfNews, Model model) {
+        model.addAttribute("title", typeOfNews);
+        Iterable<PostNews> postNews = postNewsRepository.findByTitleContainingOrAnonsContaining("#" + typeOfNews, "#" + typeOfNews);
+        model.addAttribute("typeNews", postNews);
+        return "news-header";
     }
 
-    @PostMapping("/newsAdd")
-    public String newsPostAdd(@RequestParam String title,@RequestParam String anons,@RequestParam String fullText, Model model){
-        PostNews postNews = PostNews.builder().title(title).anons(anons).fullText(fullText).build();
+    @GetMapping("/news/add")
+    public String newsAdd(Model model) {
+
+        return "news-add";
+    }
+
+    @GetMapping("/reading/{id}")
+    public String newsDetails(@PathVariable(name = "id") long id, Model model) {
+        if (postNewsRepository.existsById(id)) {
+            PostNews postNews = postNewsRepository.findById(id).orElse(null);
+            model.addAttribute("news", postNews);
+            return "news-details";
+        } else return "news-error";
+    }
+
+    @GetMapping("/aboutMe")
+    public String aboutMe(Model model) {
+        model.addAttribute("title", "About Me");
+        return "about-me";
+    }
+
+    @PostMapping("/news/add")
+    public String newsPOSTAdd(@RequestParam String title, @RequestParam String anons, @RequestParam String fullText, Model model) {
+        PostNews postNews = PostNews.builder().title(title + " #").anons(anons).fullText(fullText).build();
+        postNewsRepository.save(postNews);
+        return "redirect:/";
+    }
+    @PostMapping("/news/delete/{id}")
+    public String deleteNews(@PathVariable(name = "id") long id) {
+        if (!postNewsRepository.existsById(id)) {
+            return "news-error";
+        }
+        postNewsRepository.deleteById(id);
+        return "redirect:/";
+    }
+
+    @GetMapping("/news/edit/{id}")
+    public String editGETNews(@PathVariable(name = "id") long id, Model model) {
+        if (!postNewsRepository.existsById(id)) {
+            return "news-error";
+        }
+        PostNews postNews = postNewsRepository.findById(id).orElse(null);
+        model.addAttribute("news",postNews);
+        return "news-edit";
+    }
+    @PostMapping("/news/edit/{id}")
+    public String editNews(@PathVariable(name = "id") long id, @RequestParam String title, @RequestParam String anons, @RequestParam String fullText, Model model) {
+        PostNews postNews = postNewsRepository.findById(id).orElseThrow();
+        postNews.setTitle(title);
+        postNews.setAnons(anons);
+        postNews.setFullText(fullText);
         postNewsRepository.save(postNews);
         return "redirect:/";
     }
 }
+
